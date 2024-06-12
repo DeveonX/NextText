82d95e2ee5ee1722048d4bd385832f9611cd7e94
EEVEE4U <143544942+EEVEE4U@users.noreply.github.com>
2024-06-13 00:58:16 +0530

    added express

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,17 @@
 import http from 'http';
 import SocketService from './services/socket';
+const express = require('express')
+import { Request, Response } from 'express';
 
 async function init(){
-
+  const app = express()
   const socketService = new SocketService();
 
-  const httpServer = http.createServer()
+  const httpServer = http.createServer(app)
+
+  app.get('/', (req: Request, res: Response)=>{
+    res.send("Server is running")
+  })
   const PORT = process.env.PORT ? process.env.PORT : 8000
 
   socketService.io.attach(httpServer)
